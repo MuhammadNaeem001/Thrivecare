91e21c9daceb949238b1f0b378d4aceeb9c2061e
MuhammadNaeem001 <nk510448@gmail.com>
2025-04-17 15:10:43 +0500

    sign-up/verify-email page done

---
+++ src/app/components/signup-form.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+function SignupForm() {
+  return (
+    <div className='pl-3 pr-3 pt-6'>
+      {/* Main Heading */}
+      <div className='text-[30px] font-bold mb-6'>
+        <h1 className='font-Poppins'>Create Account</h1>
+      </div>
+
+      {/* Full Name Field */}
+      <div className='mb-4'>
+        <label className=" font-Poppins block text-sm font-bold mb-1 h-[26px] text-[16px] leading-[26px]">Full Name<span className='text-red-600'>*</span></label>
+        <input
+          type='text'
+          placeholder='Babar Sandhu'
+           className=' font-Poppins w-[525px] h-[48px]  gap-[8px] p-3 border border-[#A9A9A9] rounded-md bg-[#EDEEF2] outline-none'
+        />
+      </div>
+
+      {/* Email Field */}
+      <div className='mb-4'>
+        <label className=" font-Poppins block text-sm font-bold mb-1 h-[26px] text=[16px]leading-[26px]">Email<span className='text-red-600'>*</span></label>
+        <input
+          type='email'
+          placeholder='name@example.com'
+          className=' font-Poppins w-[525px] h-[48px]  gap-[8px] p-3 border border-[#A9A9A9] rounded-md bg-[#EDEEF2] outline-none '
+        />
+      </div>
+
+      {/* Phone Number Field */}
+      <div className='mb-4'>
+        <label className=" font-Poppins block text-sm font-bold mb-1 h-[26px] text-[16px]leading-[26px]">Phone Number<span className='text-red-600'>*</span></label>
+        <input
+          type='tel'
+          placeholder='(937) 768 0987'
+           className=' font-Poppins w-[525px] h-[48px]  gap-[8px] p-3 border border-[#A9A9A9] rounded-md bg-[#EDEEF2] outline-none'
+        />
+      </div>
+
+      {/* Password Field */}
+      <div className='mb-4'>
+        <label className=" font-Poppins block text-sm font-bold mb-1 h-[26px] text-[16px] leading-[26px]">Password<span className='text-red-600'>*</span></label>
+        <input
+          type='password'
+          placeholder='**********'
+        className=' font-Poppins w-[525px] h-[48px]  gap-[8px] p-3 border border-[#A9A9A9] rounded-md bg-[#EDEEF2] outline-none '
+        />
+      </div>
+
+      {/* Submit Button */}
+      <button className=" font-Poppins w-[525px] h-[48px] gap-[24px] bg-[#108572] hover:bg-[#108572] text-[#FFFFFF] font-bold[600] text-[16px] py-3 rounded-md mt-4">
+        Create
+      </button>
+      
+  {/* Text below button */}
+  <p className=" font-Poppins text-[16px] font-medium mt-5">
+    Already Have an Account? <span className="text-[#696F79] font-bold[500]  text-[16px]cursor-pointer">Login</span>
+  </p>
+    </div>
+  );
+}
+
+export default SignupForm;
---
+++ src/app/sign-up/page.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+import loginImg from '../../../public/images/login-img.png';
+import Logo from '../../../public/images/Group.png';
+import Signup from '../components/signup-form';
+
+function Page() {
+  return (
+    
+    <div className=" flex flex-col">
+      <div className="flex h-full">
+        {/* Left Image */}
+        <div className="w-[720px] h-[1024px] ">
+          <Image 
+            src={loginImg} 
+            alt="Login Image" 
+            className="object-cover w-[100%] h-full" 
+            width={0} 
+            height={0} 
+          />
+        </div>
+
+        {/* Right Logo - Centered and smaller */}
+        <div className="w-1/2 h-full">
+        {/* inner container  */}
+            <div className='flex flex-col  pt-5  px-20'>
+
+        <div>
+
+          <Image 
+            src={Logo} 
+            alt="Logo" 
+            className="object-contain w-[250px] h-{100px} mx-auto"  // Smaller logo
+            width={0} 
+            height={0} 
+            />
+            </div>
+
+<div className='items-start'> <Signup/></div >
+
+</div>
+
+
+        </div>
+        
+      </div>
+    </div>
+    
+  );
+}
+
+export default Page;
